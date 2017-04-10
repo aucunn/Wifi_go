7de144817acd71d7a2f029099a01f2b4baa08698
aucun <aucun@naver.com>
2017-04-10 22:24:15 +0900

    Rejected commit: Default

--- app/src/main/java/com/mysterlee/www/wifi_go/MainActivity.java
+++ app/src/main/java/com/mysterlee/www/wifi_go/MainActivity.java
@@ -1,7 +1,11 @@
 package com.mysterlee.www.wifi_go;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.mysterlee.www.MapsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        EditText editId = (EditText)findViewById(R.id.editId);
+        EditText editPass = (EditText)findViewById(R.id.editPass);
+        Button btnLogin = (Button)findViewById(R.id.buttonLogin);
+
+        editId.setText("테스트 입니다.");
+        */
+
+    }
+
+    public void onClickLogin(View v) {
+        Intent intent_01 = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(intent_01);
     }
 }
