eac68740715197cd7fd03ef71fc5e7f85ae86785
unknown <>
2017-04-10 14:29:04 +0900

    Initial commit

---
+++ app/src/main/java/com/mysterlee/www/wifi_go/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mysterlee.www.wifi_go;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
