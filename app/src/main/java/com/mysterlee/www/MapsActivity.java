7d70341078ada8e3b428576f15979daff08b34c7
aucun <aucun@naver.com>
2017-04-23 19:38:05 +0900

    지도 위치 완료

--- app/src/main/java/com/mysterlee/www/MapsActivity.java
+++ app/src/main/java/com/mysterlee/www/MapsActivity.java
@@ -1,31 +1,64 @@
 package com.mysterlee.www;
 
 import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 import com.mysterlee.www.wifi_go.R;
 
 import java.util.ArrayList;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    private boolean mLocationPermission = false;
+
+    private LocationRequest mLocetionRequset;
+    private Location mCurrentLocation;
+    private static final String CAMERA_POSITION = "camra_position";
+    private static final String LOCATION = "location";
+    private CameraPosition mCameraPosition;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null){
+            mCurrentLocation = savedInstanceState.getParcelable(LOCATION);
+            mCameraPosition = savedInstanceState.getParcelable(CAMERA_POSITION);
+        }
+
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        buildGoogleApiClient();
+        mGoogleApiClient.connect();
+
+
+
     }
 
 
@@ -42,22 +75,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        new TedPermission(this)
-                .setPermissionListener(permissionlistener)
-                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
-                .setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION)
-                .check();
+        if (mCameraPosition != null){
+            mMap.moveCamera(CameraUpdateFactory.newCameraPosition(mCameraPosition));
+        }
+        else if (mCurrentLocation != null){
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(mCurrentLocation.getLatitude(),
+                    mCurrentLocation.getLongitude()), 16));
+        }
+        else {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(25.3, 34.3), 16));
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+
+
+
+        updateLocationUI();
 
 
         // Add a marker in Sydney and move the camera
 
     }
 
+    @SuppressWarnings("MissingPermission")
     PermissionListener permissionlistener = new PermissionListener() {
         @Override
         public void onPermissionGranted() {
             Toast.makeText(MapsActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
-            mMap.setMyLocationEnabled(true);
+
+            mLocationPermission = true;
+            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            //LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocetionRequset, this);
         }
 
         @Override
@@ -68,6 +116,104 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     };
 
+    protected synchronized void buildGoogleApiClient(){
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .enableAutoManage(this, this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        createLocationRequest();
+    }
 
+
+    @SuppressWarnings("MissingPermission")
+    private void updateLocationUI() {
+
+        if (mMap == null) return;
+
+        if (mLocationPermission){
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        }
+        else{
+            mMap.setMyLocationEnabled(false);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
+
+    private void createLocationRequest(){
+        mLocetionRequset = new LocationRequest();
+        mLocetionRequset.setInterval(10000); //ms
+        mLocetionRequset.setFastestInterval(5000);
+        mLocetionRequset.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); //위치정확도 or 배터리 중요도
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        new TedPermission(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mCurrentLocation = location;
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+
+        if (mMap != null){
+            outState.putParcelable(CAMERA_POSITION, mMap.getCameraPosition());
+            outState.putParcelable(LOCATION, mCurrentLocation);
+            super.onSaveInstanceState(outState);
+        }
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if (mGoogleApiClient.isConnected()){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        if (mGoogleApiClient.isConnected()){
+            new TedPermission(this)
+                    .setPermissionListener(permissionlistener)
+                    .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                    .setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION)
+                    .check();
+        }
+        super.onResume();
+    }
 
 }
