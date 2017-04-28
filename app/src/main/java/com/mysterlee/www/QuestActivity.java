9bbd479322be793cc8944eaa93b29634b6bed4e6
aucun <aucun@naver.com>
2017-04-28 22:23:56 +0900

    퀘스트 화면 제작중...

---
+++ app/src/main/java/com/mysterlee/www/QuestActivity.java
@@ -0,0 +1,15 @@
+package com.mysterlee.www;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mysterlee.www.wifi_go.R;
+
+public class QuestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest);
+    }
+}
